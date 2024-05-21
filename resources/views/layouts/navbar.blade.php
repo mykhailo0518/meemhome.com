37dd84dadb5e173af7bd28e60a00c01f5de3dc33
mykhailo0518 <khvlinskyimykhailo@gmail.com>
2024-05-21 13:36:55 -0700

    fix: change nav icon color and hover

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -36,7 +36,7 @@
             <a class="nav-link cursor-pointer ps-3 d-block d-md-none" data-bs-toggle="modal"
                 data-bs-target="#searchModal">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" style="width:1.5rem;height:1.5rem;">
+                    stroke="currentColor" style="color:#3fa2b7; width:1.5rem; height:1.5rem;">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
@@ -44,7 +44,7 @@
             @guest
                 <a href="{{ route('login') }}" class="nav-link ps-3 d-block d-md-none">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" style="width:1.5rem;height:1.5rem;">
+                        stroke="currentColor" style="color:#5687af; width:1.5rem; height:1.5rem;">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                     </svg>
@@ -132,10 +132,10 @@
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
 
 
-                    <li class="nav-item ps-3">
-                        <a class="nav-link cursor-pointer px-0" data-bs-toggle="modal" data-bs-target="#searchModal">
+                    <li class="nav-item ps-2">
+                        <a class="nav__icon nav-link cursor-pointer px-0" data-bs-toggle="modal" data-bs-target="#searchModal">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" style="width:1.5rem;height:1.5rem;">
+                                stroke-width="1.5" stroke="currentColor" style="color:#3fa2b7; width:1.5rem;height:1.5rem;">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                             </svg>
@@ -143,10 +143,10 @@
                     </li>
 
                     @guest
-                        <li class="nav-item ps-3">
-                            <a class="nav-link px-0" href="{{ route('login') }}">
+                        <li class="nav-item ps-2">
+                            <a class="nav__icon nav-link px-0" href="{{ route('login') }}">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" style="width:1.5rem;height:1.5rem;">
+                                    stroke-width="1.5" stroke="currentColor" style="color:#5687af;width:1.5rem;height:1.5rem;">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                                 </svg>
@@ -154,12 +154,12 @@
                             </a>
                         </li>
                     @endguest
-                    <li class="nav-item ps-3">
-                        <a class="nav-link px-0 @if (Route::currentRouteName() == 'cart') active @endif"
+                    <li class="nav-item ps-2">
+                        <a class="nav__icon nav-link px-0 @if (Route::currentRouteName() == 'cart') active @endif"
                             href="{{ route('cart') }}">
                             <div class="position-relative">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" style="width:1.5rem;height:1.5rem;">
+                                    stroke-width="1.5" stroke="currentColor" style="color:orange;width:1.5rem;height:1.5rem;">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                 </svg>
