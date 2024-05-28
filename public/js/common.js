551b8255336ec9a333611e2848d166aff5ebd5fb
mykhailo0518 <khvlinskyimykhailo@gmail.com>
2024-05-28 10:18:21 -0700

    fix: clean up codebase

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Your code here
-    console.log('Document is ready!');
-});
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/common.js":
+/*!********************************!*\
+  !*** ./resources/js/common.js ***!
+  \********************************/
+/***/ (() => {
+
+eval("document.addEventListener('DOMContentLoaded', function () {\n  // Your code here\n  console.log('Document is ready!');\n  console.log(\"asdfasdf\");\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9yZXNvdXJjZXMvanMvY29tbW9uLmpzLmpzIiwibmFtZXMiOlsiZG9jdW1lbnQiLCJhZGRFdmVudExpc3RlbmVyIiwiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vcmVzb3VyY2VzL2pzL2NvbW1vbi5qcz84NmZmIl0sInNvdXJjZXNDb250ZW50IjpbImRvY3VtZW50LmFkZEV2ZW50TGlzdGVuZXIoJ0RPTUNvbnRlbnRMb2FkZWQnLCBmdW5jdGlvbigpIHtcclxuICAgIC8vIFlvdXIgY29kZSBoZXJlXHJcbiAgICBjb25zb2xlLmxvZygnRG9jdW1lbnQgaXMgcmVhZHkhJyk7XHJcbiAgICBjb25zb2xlLmxvZyhcImFzZGZhc2RmXCIpXHJcbn0pOyJdLCJtYXBwaW5ncyI6IkFBQUFBLFFBQVEsQ0FBQ0MsZ0JBQWdCLENBQUMsa0JBQWtCLEVBQUUsWUFBVztFQUNyRDtFQUNBQyxPQUFPLENBQUNDLEdBQUcsQ0FBQyxvQkFBb0IsQ0FBQztFQUNqQ0QsT0FBTyxDQUFDQyxHQUFHLENBQUMsVUFBVSxDQUFDO0FBQzNCLENBQUMsQ0FBQyJ9\n//# sourceURL=webpack-internal:///./resources/js/common.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/js/common.js"]();
+/******/ 	
+/******/ })()
+;
